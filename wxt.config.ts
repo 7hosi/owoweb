16f23bab5fe54e4e06a9eceaa6956df813dbfe17
nanahoshi <nanahosi@proton.me>
2026-03-11 01:15:05 +0300

    Add more details to manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
   srcDir: "src",
   modules: ["@wxt-dev/module-solid"],
   manifest: {
+    description: "Send images to owocr and display text boxes overlayed on the image.",
+    homepage_url: "https://github.com/7hosi/owoweb",
     host_permissions: ["<all_urls>"],
     permissions: ["storage"],
   },
