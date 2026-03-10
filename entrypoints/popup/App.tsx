c384d5119cb1395cc1dc145c0a6b4d287f90034a
nanahoshi <nanahosi@proton.me>
2026-03-10 15:11:53 +0300

    Add prettier and run format

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,10 +1,10 @@
-import { createSignal } from 'solid-js';
-import solidLogo from '@/assets/solid.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
+import { createSignal } from "solid-js"
+import solidLogo from "@/assets/solid.svg"
+import wxtLogo from "/wxt.svg"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = createSignal(0);
+  const [count, setCount] = createSignal(0)
 
   return (
     <>
@@ -18,18 +18,14 @@ function App() {
       </div>
       <h1>WXT + Solid</h1>
       <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count()}</button>
         <p>
           Edit <code>popup/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p class="read-the-docs">
-        Click on the WXT and Solid logos to learn more
-      </p>
+      <p class="read-the-docs">Click on the WXT and Solid logos to learn more</p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
