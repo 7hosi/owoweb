d2ba0472e79e4e11e29cc2a05a9e616f3c3c60fc
nanahoshi <nanahosi@proton.me>
2026-03-11 00:11:09 +0300

    Add AnkiConnect settings

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -5,3 +5,15 @@ export type OwocrSettings = {
 export const defaultOwocrSettings: OwocrSettings = {
   websocketUrl: "ws://localhost:7331",
 }
+
+export type AnkiConnectSettings = {
+  ankiConnectUrl: string
+  noteType: string
+  pictureField: string
+}
+
+export const defaultAnkiConnectSettings: AnkiConnectSettings = {
+  ankiConnectUrl: "http://127.0.0.1:8765",
+  noteType: "",
+  pictureField: "",
+}
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,5 +1,13 @@
 import { defaultOwocrSettings, OwocrSettings } from "~/utils/settings"
+import { defaultAnkiConnectSettings, AnkiConnectSettings } from "~/utils/settings"
 
 export const owocrSettingsStore = storage.defineItem<OwocrSettings>("local:owocr-settings", {
   fallback: defaultOwocrSettings,
 })
+
+export const ankiConnectSettingsStore = storage.defineItem<AnkiConnectSettings>(
+  "local:ankiconnect-settings",
+  {
+    fallback: defaultAnkiConnectSettings,
+  },
+)
